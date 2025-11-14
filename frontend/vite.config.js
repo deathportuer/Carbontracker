21e8bd823bcf9db718dbaff512f8578f2d003baa
deathportuer <79647966+deathportuer@users.noreply.github.com>
2025-11-14 20:10:01 +0530

    Refactor Vite config to add root and server settings
    Updated Vite configuration to include root and server port settings.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  root: "./",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   server: {
     host: true,
+    port: 5173,
   },
-  build: {
-    outDir: 'dist',
-  },
-})
+});
